3929f39c2cb72f74766a021ea627dda13038815e
Yogesh <yogesh2003kashyap@gmail.com>
2026-08-09 01:03:32 +0530

    fix user save middleware

--- src/modules/auth/user.model.js
+++ src/modules/auth/user.model.js
@@ -48,11 +48,11 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.pre(/^find/, function () {
